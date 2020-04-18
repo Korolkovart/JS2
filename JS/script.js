7d26a0c0d91a79fce66db9280b696ff477ed2fe8
Korolkovart <56297922+Korolkovart@users.noreply.github.com>
2020-04-18 13:37:02 +0300

    1
    1 les

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+const goods = [
+    { image: 'photo', title: 'Shirt', price: 150},
+    { image: 'photo', title: 'Socks', price: 50 },
+    { image: 'photo', title: 'Jacket', price: 350 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+    { image: 'photo', title: 'Shoes', price: 250 },
+  ];
+  
+const renderGoodsItem = (image, title, price) => {
+    return `<div class="goods-item">
+                  <img class="img-goods" src="${image}" alt="photo-goods">
+                <div class="inf-goods">
+                  <h3 class="name-goods">${title}</h3>
+                  <p class="price-goods">${price}</p>
+                  <button class="goods-button" type="button"> Добавить в корзину</button>
+                </div>
+            </div>`;
+  };
+  
+const renderGoodsList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.img, item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join("");
+  }
+  
+renderGoodsList(goods);
+  

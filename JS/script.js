b4fc5665fa249b3bc665188e6b1987d837d58e37
Korolkovart <56297922+Korolkovart@users.noreply.github.com>
2020-04-25 11:47:25 +0300

    fixed promise

--- JS/script.js
+++ JS/script.js
@@ -45,32 +45,40 @@
 // const addToCart = document.querySelector('goods-button');
 // addToCart.addEventListener("click");
 
-
 function makeGETRequest(url, callback) {
-  var xhr;
-
-  if (window.XMLHttpRequest) {
-    xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { 
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  return new Promise((resolve, reject) => {
+      let xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject;
+      xhr.open("GET", url, true);
+      xhr.onload = () => resolve(callback(xhr.responseText));
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
   }
+// function makeGETRequest(url, callback) {
+//   var xhr;
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      callback(xhr.responseText);
-    }
-  }
+//   if (window.XMLHttpRequest) {
+//     xhr = new XMLHttpRequest();
+//   } else if (window.ActiveXObject) { 
+//     xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//   }
 
-  xhr.open('GET', url, true);
-  xhr.send();
-}
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       callback(xhr.responseText);
+//     }
+//   }
+
+//   xhr.open('GET', url, true);
+//   xhr.send();
+// }
 
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 
 class GoodsItem {
-  constructor(product_name, price,) {
-    // this.image = image;
+  constructor(id_product,product_name, price,) {
+    this.id_product = id_product;
     this.product_name = product_name;
     this.price = price;
   }
@@ -79,8 +87,9 @@ class GoodsItem {
     return `<div class="goods-item">
                        <img class="img-goods" src="./img/icon-cart.svg" alt="photo-goods">
                      <div class="inf-goods">
+                      <span> #${this.id_product}</span>
                        <h3 class="name-goods">${this.product_name}</h3>
-                       <p class="price-goods">${this.price}</p>
+                       <p class="price-goods">${this.price} ₽</p>
                        <button class="goods-button" type="button"> Добавить в корзину</button>
                      </div>
                  </div>`;
@@ -92,24 +101,6 @@ class GoodsList {
     this.goods = [];
   }
   fetchGoods() {
-    // const readFile = () =>{
-    //   const promis = new Promis ((resolve, reject) => {
-
-    //     setTimeout(() => {
-
-    //       resolve(
-    //         makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-    //         this.goods = JSON.parse(goods)
-    //         })
-    //       )
-
-    //     }, 1);
-    //   })
-    //   return promis
-    // }
-        
-
-     
     makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
       this.goods = JSON.parse(goods);
     })
@@ -118,30 +109,9 @@ class GoodsList {
 
 
   render() {
-    
-
-    // const promis1 = new Promis ((resolve1, reject) => {
-
-    //   setTimeout(() => {
-
-    //     resolve1(
-    //         let listHtml = '' ;
-    //         this.goods.forEach(good => {
-    //         const goodItem = new GoodsItem(good.product_name, good.price,);
-    //         listHtml += goodItem.render();
-    //         });
-    //         document.querySelector('.goods-list').innerHTML = listHtml;
-    //         )
-        
-    //   }, 1000);
-  
-    // });
-
-
-
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.product_name, good.price);
+      const goodItem = new GoodsItem(good.id_product, good.product_name, good.price);
       listHtml += goodItem.render();
     })
     document.querySelector('.goods-list').innerHTML = listHtml;
@@ -160,29 +130,28 @@ class GoodsList {
 }
 
 class CartItem{
-  constructor(product_name, price,) {
-    // this.image = image;
+  constructor(id_product, product_name, price,) {
+    this.id_product = id_product;
     this.product_name = product_name;
     this.price = price;
   }
 
   render() {
     return `<div class="basket-item">
+            <span>${this.id_product}</span>
             <h2 class="title-basket-item">${this.product_name}</h2>
-            <span class="price-basket-item">${this.price}</span>
+            <span class="price-basket-item">${this.price} ₽</span>
             <button class="cart-button" type="button">Удалить</button>
             </div>`;
   };
 }
-
-
 
 class Cart{
   constructor(){
     this.goods = [];
   }
   getCart(){
-      makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
+      makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
       this.goods = JSON.parse(goods);
       console.log(this.goods)
     })
@@ -191,20 +160,16 @@ class Cart{
   renderCart(){
       let cartHtml = '';
       this.goods.forEach(good => {
-      const cartItem = new CartItem(good.product_name, good.price);
+      const cartItem = new CartItem(good.id_product, good.product_name, good.price);
       cartHtml += cartItem.render();
     })
     document.querySelector('.basket').innerHTML = cartHtml;
   }
-  
-
-
-  
   moveCart(){
     let activeBasket = document.querySelector('.basket');
     activeBasket.classList.toggle('active-basket');
   }
-  renderElem(){
+  renderMoveCart(){
     let elem = document.querySelector('.cart-button')
     elem.addEventListener("click", () => this.moveCart());
     
@@ -234,7 +199,9 @@ class Cart{
 
 
 const list = new GoodsList();
+
 list.fetchGoods();
+
 
 setTimeout(() => { list.render()}, 1000);
 setTimeout(() => {list.renderTotalPrice()}, 1000);
@@ -244,10 +211,9 @@ const cart = new Cart();
 cart.getCart()
 setTimeout(() => {cart.renderCart()}, 1000); 
 
-
-cart.renderElem()
-
+cart.renderMoveCart()
 
 
-// const basket = new cartItem();
+
+// const basket = new CartItem();
 // basket.render()

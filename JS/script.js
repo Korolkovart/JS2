6097404f81a2a938031a1bf6fa46f0ffb2a3dba4
Korolkovart <56297922+Korolkovart@users.noreply.github.com>
2020-04-29 01:15:03 +0300

    added method addItemCart to class Cart

--- JS/script.js
+++ JS/script.js
@@ -89,8 +89,8 @@ class GoodsItem {
                      <div class="inf-goods">
                       <span> #${this.id_product}</span>
                        <h3 class="name-goods">${this.product_name}</h3>
-                       <p class="price-goods">${this.price} ₽</p>
-                       <button class="goods-button" type="button"> Добавить в корзину</button>
+                       <p class="price-goods">${this.price.toLocaleString()} ₽</p>
+                       <button data-art="'+this.id_product+'" class="goods-button" type="button" onclick="cart.renderAddItemCart()"> Добавить в корзину</button>
                      </div>
                  </div>`;
   };
@@ -105,9 +105,6 @@ class GoodsList {
       this.goods = JSON.parse(goods);
     })
   }
-
-
-
   render() {
     let listHtml = '';
     this.goods.forEach(good => {
@@ -127,7 +124,11 @@ class GoodsList {
     let totalPriceCart = `Сумма:   ${this.calculateTotalPrice()}`;
     document.querySelector('.total-price').innerHTML = totalPriceCart;
   }
+
+
+
 }
+
 
 class CartItem{
   constructor(id_product, product_name, price, quantity) {
@@ -141,7 +142,7 @@ class CartItem{
     return `<div class="basket-item">
             <span>#${this.id_product}</span>
             <h2 class="title-basket-item">${this.product_name}</h2>
-            <span class="price-basket-item">${this.price} ₽</span>
+            <span class="price-basket-item">${this.price.toLocaleString()} ₽</span>
             <span class="quantity-basket-item">${this.quantity} шт</span>
             <button class="cart-button" type="button">Удалить</button>
             </div>`;
@@ -152,10 +153,28 @@ class Cart{
   constructor(){
     this.goods = [];
   }
+  addItemCart(id_product){
+    let addItem;
+    list.goods.forEach(function(item){
+      if(id_product = item.id_product) {
+        addItem = {
+          id_product: item.id_product,
+          product_name: item.product_name,
+          price: item.price,
+          quantity: item.quantity
+        }
+      }
+    });
+    this.goods.push(addItem)
+  }
+  renderAddItemCart(){
+    let addItemtoCart = this.addItemCart()
+  }
+
   getCart(){
       makeGETRequest(`${API_URL}/getBasket.json`, (goods) => {
       this.goods = JSON.parse(goods).contents;
-      console.log(this.goods.contents)
+      console.log(this.goods)
     })
   }
 
@@ -174,14 +193,20 @@ class Cart{
   renderMoveCart(){
     let elem = document.querySelector('.cart-button')
     elem.addEventListener("click", () => this.moveCart());
+
     
   }
-
-  addItemCart(){
-    //добавляем товары 
+  qwer(){
+    // let addItem = document.querySelector('.goods-button')
+    // addItem.addEventListener('click', () => console.log(`${addItem.id}  push button`))
+    // console.log(`addItem push button`)
+    // let a = this.goods.find(item => item.id == 1);
+    // console.log(this.goods.product_name)
   }
-  render(){
+  renderQwer(){
+    // console.log(addItem + ' push button')
     //отрисовка корзины  
+    // addItem.addEventListener('click', () => console.log(addItem + ' push button'))
   }
   delItemCart(){
     //удаляем товары 
@@ -214,8 +239,13 @@ cart.getCart()
 setTimeout(() => {cart.renderCart()}, 1000); 
 
 cart.renderMoveCart()
+// cart.renderAddItemCart()
 
+cart.renderAddItemCart()
 
 
 // const basket = new CartItem();
 // basket.render()
+
+
+
